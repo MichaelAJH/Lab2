a27d89733b47507351537de0a6cfb20b2d1185ca
MichaelAJH <jh.ahn.michael@gmail.com>
2026-04-01 23:00:01 +0900

    solved

--- main.cpp
+++ main.cpp
@@ -4,19 +4,29 @@
 using namespace std;
 
 int N,M;
-vector<int> vec;
+int arr[8];
 
-void func(int cnt) {
+void printArr(const int* arr, int length) {
+   for (int i=0; i<length; i++)
+      cout << arr[i] + 1;
+   cout << endl;
+}
 
-
-
-
-
-
-
+void func(int* arr, int n, int m) {
+   if (m == M - 1) {
+      for (int i=n; i<N; i++) {
+         arr[m] = i;
+         printArr(arr, M);
+      }
+   } else {
+      for (int i=n; i<N-m-1; i++) {
+         arr[m] = i;
+         func(arr, i+1, m+1);
+      }
+   }
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(arr, 0, 0);
 }
